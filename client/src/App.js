580e1d2d6df21a22f9297342b02e049647a2ed22
Orange4Broom <orang4broom@gmail.com>
2022-11-04 19:30:14 +0100

    Fe: Sending data to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,40 @@ import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [languages, setLanguages] = useState([]);
+  const [activeLanguages, setActiveLanguages] = useState([]);
+
+  const [technologies, setTechnologies] = useState([]);
+  const [activeTechnologies, setActiveTechnologies] = useState([]);
 
   const firstNameRef = useRef();
   const lastNameRef = useRef();
   const localityRef = useRef();
   const schoolRef = useRef();
   const endYearRef = useRef();
+  const gdprRef = useRef();
+
+  const toggleLanguageActive = (id) => {
+    activeLanguages.includes(id)
+      ? setActiveLanguages(activeLanguages.filter(language => language !== id))
+      : setActiveLanguages([...activeLanguages, id]);
+  }
 
   useEffect(() => {
     fetch('http://192.168.43.201:3001/language')
       .then(res => res.json())
       .then(data => setLanguages(data));
+  }, []);
+
+  const toggleTechnologyActive = (id) => {
+    activeTechnologies.includes(id)
+    ? setActiveTechnologies(activeTechnologies.filter(technology => technology !== id))
+    : setActiveTechnologies([...activeTechnologies, id]);
+  }
+
+  useEffect(() => {
+    fetch('http://192.168.43.201:3001/technology')
+      .then(res => res.json())
+      .then(data => setTechnologies(data));
   }, []);
 
   const handleSubmit = (e) => {
@@ -27,12 +50,13 @@ function App() {
         locality: localityRef.current.value,
         school: schoolRef.current.value,
         end_year: endYearRef.current.value,
-        languages: "1,8",
-        technologies: "1,3",
-        gdpr: true,
+        languages: activeLanguages.join(','),
+        technologies: activeTechnologies.join(','),
+        gdpr: gdprRef.current.checked,
       })
     });
   }
+
 
   return (
     <div className="App">
@@ -50,14 +74,25 @@ function App() {
         <p>Jazyky:</p>
         {languages.map(language => (
           <div key={language.language_id}>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={() => toggleLanguageActive(language.language_id)} />
             <label>{language.name}</label>
           </div>
         ))}
+        <p>Technologie:</p>
+        {technologies.map(technology => (
+          <div key={technology.technology_id}>
+            <input type="checkbox" onChange={() => toggleTechnologyActive(technology.technology_id)} />
+            <label>{technology.name}</label>
+          </div>
+        ))}
+        <p>Souhlasím s gdpr: <input type="checkbox" name='gdpr' ref={gdprRef}/></p>
+        <br />
         <button>Submit</button>
       </form>
     </div>
   );
+
 }
+
 
 export default App;
