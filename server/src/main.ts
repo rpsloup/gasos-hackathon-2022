55b45d7942fb7c6fe9478ee4f650f53eec087a17
rpsloup <robin.patrik.sloup@proton.me>
2022-11-04 14:23:35 +0100

    BE: Adding and setting up Postgres

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+dotenv.config();
+export const pool = new Pool({
+  host: process.env.DB_HOST ?? '',
+  user: process.env.DB_USER ?? '',
+  password: process.env.DB_pASS ?? '',
+  database: process.env.DB_NAME ?? '',
+  port: Number(process.env.DB_PORT) ?? '',
+});
 
 const DEFAULT_PORT: number = 3001;
 const port = process.env.port || DEFAULT_PORT;
