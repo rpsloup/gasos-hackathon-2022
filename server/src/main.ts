94fc7bcba7376488a5b35fccd89c7e80d30ee834
rpsloup <robin.patrik.sloup@proton.me>
2022-11-04 13:45:14 +0100

    BE: Adding and setting up Express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,15 @@
-console.log('Initial, commit!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DEFAULT_PORT: number = 3001;
+const port = process.env.port || DEFAULT_PORT;
+
+app.get('/', (_, res) => res.json('BE funguje.'));
+
+app.listen(port, () => {
+  console.log(`[Start] Listening on port ${port}`);
+});
