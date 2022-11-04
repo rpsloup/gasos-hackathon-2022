9d2e83db6b879d522530db3ce4b9e984a4fe7f2a
rpsloup <robin.patrik.sloup@proton.me>
2022-11-04 16:04:41 +0100

    BE: Improving GET response from the /student route

--- server/src/routes/studentRoutes.ts
+++ server/src/routes/studentRoutes.ts
@@ -7,7 +7,17 @@ const studentRouter = Router();
 studentRouter.get('/student', async (_, res) => {
   try {
     const students = await pool.query('SELECT * FROM Students');
-    res.json(students?.rows ?? []);
+    const fetchedLanguages = await pool.query('SELECT * FROM Languages');
+    const fetchedTechnologies = await pool.query('SELECT * FROM Technologies');
+    if (!students?.rows) res.send([]);
+    
+    res.send(
+      students.rows.map(({ languages, technologies, ...restProperties }) => ({
+        ...restProperties,
+        languages: fetchedLanguages?.rows && languages ? fetchedLanguages.rows.filter(language => languages.split(',').includes(String(language.language_id))) : [],
+        technologies: fetchedTechnologies?.rows && technologies ? fetchedTechnologies.rows.filter(technology => technologies.split(',').includes(String(technology.technology_id))) : [],
+      }))
+    );
   } catch (error) {
     console.log(error);
     res.status(500).json('Error!');
