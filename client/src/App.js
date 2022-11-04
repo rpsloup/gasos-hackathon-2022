8290771a9c71ea284a00e1871e6113de63b5d014
rpsloup <robin.patrik.sloup@proton.me>
2022-11-05 00:04:38 +0100

    FE: Creating the Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () =>
   (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/admin" element={<AdminPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+const LoginPage = () =>
+  (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+
+export default LoginPage;
