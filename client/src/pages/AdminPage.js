10477b9f154023cb7c05f9573cbbbd32b8951e1f
rpsloup <robin.patrik.sloup@proton.me>
2022-11-05 00:25:53 +0100

    FE: Displaying the Loader component when loading the student data

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,20 +1,27 @@
 import { useState, useEffect } from 'react';
 
+import Loader from '../components/Loader';
+
 import '../styles/reset.scss';
 
 const AdminPage = () => {
   const [students, setStudents] = useState([]);
+  const [studentsLoading, setStudentsLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://192.168.43.201:3001/student')
       .then(res => res.json())
-      .then(data => setStudents(data));
+      .then(data => {
+        setStudents(data);
+        setStudentsLoading(false);
+      });
   }, []);
 
   return (
     <>
       <h1>Administrace</h1>
       <h2>Studenti</h2>
+      {studentsLoading ? <Loader /> : null}
       {students && students.length > 0 ? students.map(
         student => (
           <div key={student.student_id}>
