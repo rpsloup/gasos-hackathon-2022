61de26b4df71fc03133579fd438b7370f2be98c4
rpsloup <robin.patrik.sloup@proton.me>
2022-11-05 01:02:20 +0100

    Sending emails from the BE

--- client/src/pages/email/AdminEmailPage.js
+++ client/src/pages/email/AdminEmailPage.js
@@ -1,8 +1,10 @@
-const AdminEmailPage = () =>
-  (
+const AdminEmailPage = () => {
+  return (
     <>
       <h1>Zasílání emailu</h1>
+      <button>Zaslat email všem studentům</button>
     </>
   );
+}
 
 export default AdminEmailPage;
