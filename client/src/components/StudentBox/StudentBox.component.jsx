45c5005ef41c6a4e1dcf13e40bd6e021a8a63353
rpsloup <robin.patrik.sloup@proton.me>
2022-11-05 02:28:34 +0100

    FE: Updating the design of the Student Box component

--- client/src/components/StudentBox/StudentBox.component.jsx
+++ client/src/components/StudentBox/StudentBox.component.jsx
@@ -1,12 +1,26 @@
+import Icon from '../Icon';
+
 import './StudentBox.styles.scss';
 
 const StudentBox = ({ student }) =>
   (
     <div className="student-box">
-      <p>{student.name}</p>
-      <p>{student.locality}</p>
-      <p>{student.school}</p>
-      <p>{student.end_year}</p>
+      <div>
+        <Icon name="user" type="fas" />
+        <span>{student.name}</span>
+      </div>
+      <div>
+        <Icon name="location-dot" type="fas" />
+        <span>{student.locality}</span>
+      </div>
+      <div>
+        <Icon name="chalkboard" type="fas" />
+        <span>{student.school}</span>
+      </div>
+      <div>
+        <Icon name="graduation-cap" type="fas" />
+        <span>{student.end_year}</span>
+      </div>
     </div>
   );
 
